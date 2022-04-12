30bbb89d4fef070751da2483d096a46b9b4d7b7e
Daniel Penas utrilla <dpenas-u@c2r7s3.42madrid.com>
2022-04-12 09:01:58 +0200

    Norm OK check

--- ft_check_arg.c
+++ ft_check_arg.c
@@ -6,7 +6,7 @@
 /*   By: dpenas-u <dpenas-u@student.42madrid>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/12 08:21:03 by dpenas-u          #+#    #+#             */
-/*   Updated: 2022/04/12 09:00:37 by dpenas-u         ###   ########.fr       */
+/*   Updated: 2022/04/12 09:01:25 by dpenas-u         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,10 +22,10 @@ int	ft_check_arg(char *argv[])
 	while (argv[++i])
 	{
 		if (!ft_strncmp(argv[i], "0", ft_strlen(argv[i]))
-				|| !ft_strncmp(argv[i], "-0", ft_strlen(argv[i])))
+			|| !ft_strncmp(argv[i], "-0", ft_strlen(argv[i])))
 			continue ;
 		else if (ft_strlen(argv[i]) >= 10
-				&& (!ft_atoi(argv[i]) || ft_atoi(argv[i]) == -1))
+			&& (!ft_atoi(argv[i]) || ft_atoi(argv[i]) == -1))
 			return (0);
 		else if (!ft_atoi(argv[i]) && ft_strlen(argv[i]))
 			return (0);
